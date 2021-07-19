8daa43cb007f2e659a3db7687093a68fa23219f9
Ben Childs <ben@benchilds.uk>
2021-07-19 11:27:01 +0100

    Custom Google font

--- src/components/tw-cta-simple-justified.js
+++ src/components/tw-cta-simple-justified.js
@@ -3,7 +3,7 @@ export default function Example() {
   return (
     <div className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <h2 className="text-3xl font-serif font-extrabold text-left tracking-tight text-gray-900 sm:text-4xl">
           <span className="block">Had enough of stuffy old insurers?</span>
           <span className="block text-indigo-600">Start insuring differently...</span>
         </h2>
