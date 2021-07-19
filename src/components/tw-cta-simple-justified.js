24786960de98579757ceca86fb6e76434987a7fc
Ben Childs <ben@benchilds.uk>
2021-07-19 11:22:44 +0100

    Started messing with colours and fonts

--- src/components/tw-cta-simple-justified.js
+++ src/components/tw-cta-simple-justified.js
@@ -4,8 +4,8 @@ export default function Example() {
     <div className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Ready to dive in?</span>
-          <span className="block text-indigo-600">Start your free trial today.</span>
+          <span className="block">Had enough of stuffy old insurers?</span>
+          <span className="block text-indigo-600">Start insuring differently...</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
@@ -13,7 +13,7 @@ export default function Example() {
               href="#"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
             >
-              Get started
+              Instant savings calculator
             </a>
           </div>
           <div className="ml-3 inline-flex rounded-md shadow">
